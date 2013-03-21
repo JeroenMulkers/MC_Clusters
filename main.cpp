2ed4a4a4e01c6ac8fc976e6882af22dc288ef524
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2013-03-21 17:27:54 +0100

    simple metropolis algorithm implementation

--- main.cpp
+++ main.cpp
@@ -1,61 +1,95 @@
 #include <iostream>
 #include "Ran1.h"
 #include <cmath>
+#include<algorithm>
 #include <vector>
 #include <tuple>
 
 const double PI = 3.1415;
 
-struct position{
+struct particle{
     double x;
     double y;
+    double step;
 };
 
-typedef std::vector< position > coordinates;
+typedef std::vector< particle > set_particles;
 
-double energy(coordinates);
+double Energy(set_particles);
+void initializeSet(set_particles &set, Ran1 &);
+
+/********************************************************/
 
 int main (int argc, char* argv[]) {
 
-    int N = 10; // Number of particles
-
-    coordinates coo;
-    coo.reserve(N);
-
     Ran1 RG(5);
 
-    // Initial random numbers
+    int N = 20;
+    set_particles set(N);
+    initializeSet(set, RG);
 
-    for(int i=0; i<N; i++){
+    double energy = Energy(set);
 
-        double a = sqrt(0.25*i*RG.getNumber());
-        double rndm = RG.getNumber();
+    int nMC = 1000;
 
-        position pos;
-        pos.x = a*cos(2*PI*rndm);
-        pos.y = a*sin(2*PI*rndm);
+    for(int iMC=0; iMC<nMC; iMC++){
 
-        coo.push_back(pos);
+        for(auto & p : set){
+
+            double x_backup = p.x;
+            double y_backup = p.y;
+
+            p.x += p.step*(2*RG.getNumber()-1);
+            p.y += p.step*(2*RG.getNumber()-1);
+
+            double newEnergy = Energy(set);
+
+            if( newEnergy < energy ){
+                energy = newEnergy;
+            } else {
+                p.x = x_backup;
+                p.y = y_backup;
+                p.step = std::max(p.step/1.2,0.1);
+            }
+        }
     }
 
-    std::cout << energy(coo) << std::endl;
+    for( auto p : set ){
+        std::cout << p.x << "\t" << p.y << std::endl;
+    }
 
     return 0;
 }
 
-double energy(coordinates coo){
+/********************************************************/
+
+void initializeSet(set_particles & set, Ran1 & RG){
+    int i = 0;
+    for( auto & p : set){
+
+        double a = sqrt(0.25*(++i)*RG.getNumber());
+        double rndm = RG.getNumber();
+
+        p.x = a*cos(2*PI*rndm);
+        p.y = a*sin(2*PI*rndm);
+        p.step = 0.2;
+
+    }
+}
+
+double Energy(set_particles set){
     //Calculates the energy of a given configuration
 
     double energy = 0;
 
-    for(auto it1 = coo.begin(); it1 != coo.end(); it1++){
+    for(auto it1 = set.begin(); it1 != set.end(); it1++){
 
-        position p1 = *it1;
+        particle p1 = *it1;
         energy += pow(p1.x,2);
         energy += pow(p1.y,2);
 
-        for(auto it2 = it1+1; it2 != coo.end(); it2++){
-            position p2 = *it2;
+        for(auto it2 = it1+1; it2 != set.end(); it2++){
+            particle p2 = *it2;
             energy += pow( pow(p1.x-p2.x,2) + pow(p1.y-p2.y,2) , -0.5);
         }
     }
