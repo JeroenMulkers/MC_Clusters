eadeaa11833ee2681a2f84dbcaae175e13226f92
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2013-03-21 15:00:29 +0100

    add energy function

--- main.cpp
+++ main.cpp
@@ -13,9 +13,11 @@ struct position{
 
 typedef std::vector< position > coordinates;
 
+double energy(coordinates);
+
 int main (int argc, char* argv[]) {
 
-    int N = 100; // Number of particles
+    int N = 10; // Number of particles
 
     coordinates coo;
     coo.reserve(N);
@@ -36,11 +38,27 @@ int main (int argc, char* argv[]) {
         coo.push_back(pos);
     }
 
-    for(int i=0; i<N; i++){
-        std::cout << coo[i].x << "\t" << coo[i].y << std::endl;
-    }
+    std::cout << energy(coo) << std::endl;
 
     return 0;
 }
 
+double energy(coordinates coo){
+    //Calculates the energy of a given configuration
 
+    double energy = 0;
+
+    for(auto it1 = coo.begin(); it1 != coo.end(); it1++){
+
+        position p1 = *it1;
+        energy += pow(p1.x,2);
+        energy += pow(p1.y,2);
+
+        for(auto it2 = it1+1; it2 != coo.end(); it2++){
+            position p2 = *it2;
+            energy += pow( pow(p1.x-p2.x,2) + pow(p1.y-p2.y,2) , -0.5);
+        }
+    }
+
+    return energy;
+}
