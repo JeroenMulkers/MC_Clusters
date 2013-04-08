6e563a9bc303ea4c01a5d0e3b6fb011bcbbc7c1a
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2013-04-08 12:52:39 +0200

    test 2D-dim smoothness

--- testRandGen.cpp
+++ testRandGen.cpp
@@ -9,27 +9,48 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include "Ran1.h"
+#include <string>
 
+void picture2D(const int N, const int seed, const std::string filename);
+void testPeriod(const int N, const int seed);
 int findPeriod(std::vector<double>);
 
 int main (int argc, char* argv[]) {
 
-    Ran1 RG(5);
-    int N = 100;
-
-    std::vector<double> list;
-
-    for(int i=0; i<N; i++){
-        double number = RG.getNumber();
-        std::cout << number << std::endl;
-        list.push_back(number);
-    }
-
-    std::cout << findPeriod(list) << std::endl;
+    testPeriod(1000000,5);
+    picture2D(1000,5,"random2D.dat");
 
     return 0;
+}
+
+void testPeriod(const int N, const int seed){
+
+    Ran1 RG(seed);
+
+    std::vector<double> list;
+    for(int i=0; i<N; i++)
+        list.push_back(RG.getNumber());
+
+    std::cout << findPeriod(list) << std::endl;
+}
+
+void picture2D(const int N, const int seed, const std::string filename){
+
+    Ran1 RG(seed);
+
+    std::ofstream output(filename);
+
+    for(int i=0; i<N; i++){
+        double x = RG.getNumber();
+        double y = RG.getNumber();
+        output << x << "\t" << y << std::endl;
+    }
+
+    output.close();
+
 }
 
 int findPeriod(std::vector<double> vec){
@@ -41,12 +62,10 @@ int findPeriod(std::vector<double> vec){
     for(period=1; period<vec.size(); period++){
 
         for(int i=0; i<vec.size()-period; i++){
-            if(vec[i] == vec[i+period]){
+            if(vec[i] == vec[i+period])
                 found = true;
-            } else {
-                found = false;
-                break;
-            }
+            else
+                found = false; break;
         }
 
         if (found == true) {
@@ -57,3 +76,5 @@ int findPeriod(std::vector<double> vec){
 
     return period;
 }
+
+
